eb6c6c1b1a201da3b5e5e2b6e12e6f1739a95f1b
metalmental <flupino@metalmental.net>
2025-08-10 12:17:40 +0900

    add rotation calculation service test

--- test/features/notifications/domain/services/rotation_calculation_service_test.dart
+++ test/features/notifications/domain/services/rotation_calculation_service_test.dart
@@ -2,12 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:popcal/core/utils/result.dart';
+import 'package:popcal/features/auth/domain/value_objects/user_id.dart';
 import 'package:popcal/features/calendar/domain/value_objects/date_key.dart';
 import 'package:popcal/features/notifications/domain/services/rotation_calculation_service_impl.dart';
+import 'package:popcal/features/rotation/domain/entities/rotation.dart';
 import 'package:popcal/features/rotation/domain/enums/weekday.dart';
 import 'package:popcal/features/rotation/domain/value_objects/notification_time.dart';
 import 'package:popcal/features/rotation/domain/value_objects/rotation_created_at.dart';
 import 'package:popcal/features/rotation/domain/value_objects/rotation_datetime.dart';
+import 'package:popcal/features/rotation/domain/value_objects/rotation_id.dart';
+import 'package:popcal/features/rotation/domain/value_objects/rotation_index.dart';
+import 'package:popcal/features/rotation/domain/value_objects/rotation_member_name.dart';
+import 'package:popcal/features/rotation/domain/value_objects/rotation_name.dart';
+import 'package:popcal/features/rotation/domain/value_objects/rotation_updated_at.dart';
 import 'package:popcal/shared/utils/time_utils.dart';
 
 class MockTimeUtils extends Mock implements TimeUtils {}
@@ -179,6 +186,94 @@ void main() {
       );
 
       expect(result, isTrue);
+    });
+  });
+
+  group('planUpcomingNotifications', () {
+    final futureDays = 30;
+    final from = DateTime(2025, 8, 10, 9, 5);
+    final to = from.add(Duration(days: futureDays));
+    // ローテーション曜日は、月曜日と金曜日
+    // 通知時刻は、10:00
+    // 計算開始時刻は、2025/8/10 09:05 から+30日
+    // RotationIndexは0で、user1からローテーション
+    final rotation = Rotation(
+      userId: UserId('user1'),
+      rotationId: RotationId('rotation-test'),
+      rotationName: RotationName('rotation-test'),
+      rotationMemberNames: [
+        RotationMemberName('user1'),
+        RotationMemberName('user2'),
+      ],
+      rotationDays: [Weekday.monday, Weekday.friday],
+      notificationTime: NotificationTime(TimeOfDay(hour: 10, minute: 0)),
+      currentRotationIndex: RotationIndex(0),
+      createdAt: RotationCreatedAt(DateTime(2025, 8, 10, 9, 0)),
+      updatedAt: RotationUpdatedAt(DateTime(2025, 8, 10, 9, 0)),
+    );
+
+    late RotationCalculationServiceImpl service;
+    late MockTimeUtils mockTimeUtils;
+
+    setUp(() {
+      mockTimeUtils = MockTimeUtils();
+      service = RotationCalculationServiceImpl(mockTimeUtils);
+      // スタブ設定 ※現在時刻(実行タイミングからcreatedAtやupdatedAtより少し後になる)
+      when(() => mockTimeUtils.now()).thenReturn(from);
+    });
+
+    test('通常仕様', () {
+      // テスト
+      final result = service.planUpcomingNotifications(
+        rotation: rotation,
+        futureDays: futureDays,
+      );
+      expect(result.isSuccess, isTrue);
+      final notificationSchedule = result.valueOrNull!;
+      final notificationEntry = notificationSchedule.notificationEntry;
+      final newCurrentRotationIndex =
+          notificationSchedule.newCurrentRotationIndex;
+      // RotationIndexが通知設定分+されている
+      final expectedIndex =
+          rotation.currentRotationIndex.value + notificationEntry.length;
+      expect(newCurrentRotationIndex.value, expectedIndex);
+      // 月曜日と金曜日の通知時刻が30日間分設定されている
+      int expectedNotificationEntryCount = 0;
+      for (
+        var date = from;
+        date.isBefore(to);
+        date = date.add(const Duration(days: 1))
+      ) {
+        if (rotation.rotationDays.contains(Weekday.fromDateTime(date))) {
+          expectedNotificationEntryCount++;
+        }
+      }
+      expect(notificationEntry.length, expectedNotificationEntryCount);
+      for (final entry in notificationEntry) {
+        expect(entry.notificationDate.value.hour, 10);
+        expect(entry.notificationDate.value.minute, 0);
+      }
+      // 最初のローテーション日は、user1
+      expect(notificationEntry[0].memberName.value, 'user1');
+      // 2日目のローテーション日は、user2
+      expect(notificationEntry[1].memberName.value, 'user2');
+      // 3日目のローテーション日は、user1
+      expect(notificationEntry[2].memberName.value, 'user1');
+      // 4日目のローテーション日は、user2
+      expect(notificationEntry[3].memberName.value, 'user2');
+      // メンバー名がローテーション順に割り当てられている
+      for (var i = 0; i < notificationEntry.length; i++) {
+        final expectedMemberIndex =
+            (rotation.currentRotationIndex.value + i) %
+            rotation.rotationMemberNames.length;
+        final expectedMemberName =
+            rotation.rotationMemberNames[expectedMemberIndex].value;
+        expect(notificationEntry[i].memberName.value, expectedMemberName);
+        // 通知時刻が10:00になっている
+        final dt = notificationEntry[i].notificationDate.value;
+        expect(dt.hour, 10);
+        expect(dt.minute, 0);
+      }
     });
   });
 }
